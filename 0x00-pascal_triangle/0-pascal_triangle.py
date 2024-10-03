e11b858d7ef0c5e497d65a4cb5c6ca701667a635
MarwaneMLE <khmarwane10@gmail.com>
2024-10-03 04:42:08 -0700

    Pascal Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Pascal triangle
+""" 
+def pascal_triangle(n): 
+    """
+    Pascal triangle method
+
+    Args: 
+        n(int): integer determine te size of triange
+
+    Returns:
+        list of integers representing the Pascal Triangle of n
+        returns empty list if n <= 0
+    """
+
+    K = []
+
+    if n <= 0:
+        return K
+    
+    K = [[1]]
+    for i in range(1, n):
+        temp = [1]
+        for j in range(len(K[i-1])-1):
+            current = K[i-1]
+            temp.append(K[i-1][j] + K[i-1][j+1])
+        temp.append(1)
+        K.append(temp)
+    return K
