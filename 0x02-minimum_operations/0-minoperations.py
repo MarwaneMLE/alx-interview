c9af8e050f22a11129bcf873b5012d81544f44ea
Marwane <khmarwane10@gmail.com>
2024-10-19 05:28:49 -0700

    minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
+"""
+Minimum Operations
+"""
 
 
-def minOperations(n):
+def minOperations(n: int) -> int:
+    """ Finds the minimum operations needed
+        to result in exactly nH characters in
+        a file
     """
-    calculates the fewest number of operations needed to result
-    in exactly n H characters in the file
+    available_chars = 1
+    pending_chars = n - 1
+    copied_chars = 0
+    ops = 0
 
-    Args:
-        n: integer number
-    Return: an integer n if impossible to achieve or 0 if not
-    """
-    if n < 2:
-        return 0
-    factor_list = []
-    i = 1
-    while n != 1:
-        i += 1
-        if n % i == 0:
-            while n % i == 0:
-                n /= i
-                factor_list.append(i)
-    return sum(factor_list)
+    while (pending_chars > 0):
+        if copied_chars and pending_chars % available_chars:
+            ops += 1
+        else:
+            copied_chars = available_chars
+            ops = ops + 2
+        available_chars += copied_chars
+        pending_chars -= copied_chars
+    return ops
